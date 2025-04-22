60f0d4cc8295c9e6a296985daa7e91e119e5d432
voxty <135042947+voxty@users.noreply.github.com>
2025-04-22 13:58:03 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+Config.Debug = false -- most likely want to keep this off
+
+Config.UseWhitelist = false
+
+Config.WhitelistedVehicles = {
+    "POLICE", "POLICE2", "FBI"
+}
+
+Config.TrafficLightObjects = {
+    0x3e2b73a4,
+    0x336e5e2a,
+    0xd8eba922,
+    0xd4729f50,
+    0x272244b2,
+    0x33986eae,
+    0x2323cdc5,
+    0xd8eba922,
+    0x53278b05
+}
